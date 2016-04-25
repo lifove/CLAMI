caa5c30563f86198d1d1c16f2eeacbd6102e73ad
lifove <jc@lifove.net>
2016-04-25 17:35:33 -0400

    Add Utils

---
+++ src/main/java/net/lifove/clami/util/Utils.java
@@ -0,0 +1,35 @@
+package net.lifove.clami.util;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import weka.core.Instances;
+
+public class Utils {
+	
+	/**
+	 * Load Instances from arff file. Last attribute will be set as class attribute
+	 * @param path arff file path
+	 * @return Instances
+	 */
+	public static Instances loadArff(String path,String classAttributeName){
+		Instances instances=null;
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(path));
+			instances = new Instances(reader);
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		instances.setClassIndex(instances.attribute(classAttributeName).index());
+
+		return instances;
+	}
+
+}
