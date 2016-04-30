48143b892b700bd614a2927bdc5fed42ef630e37
JC <jc@lifove.net>
2016-04-30 11:45:35 -0400

    Remove an unnecessary import statement

--- src/main/java/net/lifove/clami/util/Utils.java
+++ src/main/java/net/lifove/clami/util/Utils.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 
 import org.apache.commons.math3.stat.StatUtils;
