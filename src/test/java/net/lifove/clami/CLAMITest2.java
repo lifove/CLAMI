f70f6cc30f849069b5ec9a90966a3b288ae2c568
lifove <jc@lifove.net>
2016-06-08 15:20:38 -0400

    Update test case 2

--- src/test/java/net/lifove/clami/CLAMITest2.java
+++ src/test/java/net/lifove/clami/CLAMITest2.java
@@ -10,11 +10,31 @@ public class CLAMITest2 {
 	@Test
 	public void testRunner() {
 		
-		String[] args = {"-f","../../Documents/CDDP/CDDP/data/Relink/Zxing.arff","-l","isDefective","-p","TRUE","","--suppress"};
+		String[] args = {"-f","../../Documents/CDDP/CDDP/data/gene/httpclient.arff","-l","class","-p","buggy","","--suppress"};
 		CLAMI runner = new CLAMI();
 		runner.runner(args);
 		
-		args[6] = "-m";
+		args[1] = "../../Documents/CDDP/CDDP/data/gene/jackrabbit.arff";
+		runner.runner(args);
+		
+		args[1] = "../../Documents/CDDP/CDDP/data/gene/lucene.arff";
+		runner.runner(args);
+		
+		args[1] = "../../Documents/CDDP/CDDP/data/gene/rhino.arff";
+		runner.runner(args);
+		
+		args[1] = "../../Documents/CDDP/CDDP/data/Relink/Apache.arff";
+		args[3] = "isDefective";
+		args[5] = "TRUE";
+		runner.runner(args);
+		
+		args[1] = "../../Documents/CDDP/CDDP/data/Relink/Apache.arff";
+		runner.runner(args);
+		
+		args[1] = "../../Documents/CDDP/CDDP/data/Relink/Safe.arff";
+		runner.runner(args);
+		
+		args[1] = "../../Documents/CDDP/CDDP/data/Relink/Zxing.arff";
 		runner.runner(args);
 		
 		assertEquals(runner.posLabelValue,"TRUE");
